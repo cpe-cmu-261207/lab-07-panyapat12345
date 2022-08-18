d77944c9a8b61196d078a44f7261b53d7bdb94e3
Panyapat Wongdee <panyapat12345@gmail.com>
2022-08-18 14:46:45 +0700

    ans1

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,66 @@ import {
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isFirst, setIsFirst] = useState(true);
+  const totalTodos = todos.length;
+  const completedTodos = todos.filter((todo) => todo.completed === true).length;
+  const incompletedTodos = totalTodos - completedTodos;
 
-  const markTodo = (idx) => {};
+  useEffect(() => {
+    loadTodo();
+  }, []);
 
-  const moveUp = (idx) => {};
+  useEffect(() => {
+    if (isFirst) {
+      setIsFirst(false);
+      return;
+    }
+    saveTodo();
+  }, [todos]);
 
-  const moveDown = (idx) => {};
+  const addTodo = () => {
+    setTodos([{ title: input, completed: false }, ...todos]);
+    setInput("");
+  };
+
+  const deleteTodo = (idx) => {
+    todos.splice(idx, 1);
+    setTodos([...todos]);
+  };
+
+  const markTodo = (idx) => {
+    todos[idx].completed = !todos[idx].completed;
+    setTodos([...todos]);
+  };
+
+  const moveUp = (idx) => {
+    if (idx !== 0) {
+      const temp = todos[idx - 1];
+      todos[idx - 1] = todos[idx];
+      todos[idx] = temp;
+      setTodos([...todos]);
+    }
+  };
+
+  const moveDown = (idx) => {
+    if (idx !== todos.length - 1) {
+      const temp = todos[idx + 1];
+      todos[idx + 1] = todos[idx];
+      todos[idx] = temp;
+      setTodos([...todos]);
+    }
+  };
+
+  const saveTodo = () => {
+    localStorage.setItem("TodoList", JSON.stringify(todos));
+  };
+
+  const loadTodo = () => {
+    const todosJSON = localStorage.getItem("TodoList");
+    setTodos(JSON.parse(todosJSON));
+  };
 
   return (
     <div>
@@ -28,14 +81,48 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyUp={(event) => {
+            if (event.key != "Enter") {
+              return;
+            }
+            if (input === "") {
+              alert("Todo cannot be empty");
+              return;
+            }
+            addTodo();
+          }}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          value={input}
         />
         {/* Todos */}
+        {todos.map((todo, i) => (
+          <Todo
+            title={todo.title}
+            completed={todo.completed}
+            key={i}
+            onMark={() => {
+              markTodo(i);
+            }}
+            onDelete={() => {
+              deleteTodo(i);
+            }}
+            onMoveUp={() => {
+              moveUp(i);
+            }}
+            onMoveDown={() => {
+              moveDown(i);
+            }}
+          />
+        ))}
+
         {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
+        {/* <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
           <span className="me-auto">Todo</span>
-        </div>
+        </div> */}
         {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
+        {/* <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
           <span className="me-auto">Todo with buttons</span>
 
           <button className="btn btn-success">
@@ -50,13 +137,13 @@ export default function Home() {
           <button className="btn btn-danger">
             <IconTrash />
           </button>
-        </div>
+        </div> */}
 
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({totalTodos}) </span>
+          <span className="text-warning">Pending ({incompletedTodos}) </span>
+          <span className="text-success">Completed ({completedTodos})</span>
         </p>
 
         {/* Made by section */}
